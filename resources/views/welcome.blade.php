0090af3269c8d7fd1c252de836358a3d7c2d198a
ulkish <dev.hugomoran@gmail.com>
2019-07-22 22:20:22 -0300

    Created a PagesController, added some new routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 
-	<h1>My New Website</h1>
+	<h1>My {{ $foo }} Website</h1>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@home');
 
-Route::get('/contact', function () {
-	return view('contact');
-});
+Route::get('/about', 'PagesController@about');
 
-Route::get('/about', function () {
-	return view('about');
-});
+Route::get('/contact', 'PagesController@contact');
+
 
